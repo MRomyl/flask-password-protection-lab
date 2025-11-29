b7fb77e4a34bf3fb6c30b0bae7283512e7c22510
MRomyl <romyl.martushev@gmail.com>
2025-11-29 15:20:55 -0800

    d

--- server/app.py
+++ server/app.py
@@ -6,16 +6,79 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, UserSchema
 
-class ClearSession(Resource):
+user_schema = UserSchema()
 
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+
+        # Create new user
+        new_user = User(username=username)
+        new_user.password_hash = password  # bcrypt hashing happens in setter
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return user_schema.dump(new_user), 200
+
+
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not user.authenticate(password):
+            return {"error": "Invalid credentials"}, 401
+
+        
+        session["user_id"] = user.id
+
+        return user_schema.dump(user), 200
+
+
+
+class Logout(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop("user_id", None)
         return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
 
-if __name__ == '__main__':
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get("user_id")
+
+        if not user_id:
+            return "", 204
+
+        user = User.query.get(user_id)
+        if not user:
+            return "", 204
+
+        return user_schema.dump(user), 200
+
+
+
+class ClearSession(Resource):
+    def delete(self):
+        session["page_views"] = None
+        session["user_id"] = None
+        return {}, 204
+
+
+api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(Logout, "/logout", endpoint="logout")
+api.add_resource(CheckSession, "/check_session", endpoint="check_session")
+api.add_resource(ClearSession, "/clear", endpoint="clear")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from marshmallow import Schema, fields
 
 from config import db, bcrypt
 
+
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -10,22 +11,26 @@ class User(db.Model):
     username = db.Column(db.String)
     _password_hash = db.Column(db.String)
 
-    # Build method to protect password_hash property
     @hybrid_property
     def password_hash(self):
-        pass
+        return self._password_hash
 
-    # Build method to set password hash property using bcrypt.generate_password_hash()
     @password_hash.setter
     def password_hash(self, password):
-        pass
+        hashed = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = hashed.decode('utf-8')
 
-    # Build authenticate method that uses bcrypt.check_password_hash()
     def authenticate(self, password):
-        pass
+        if not self._password_hash:
+            return False
+        return bcrypt.check_password_hash(
+            self._password_hash.encode('utf-8'),
+            password.encode('utf-8')
+        )
 
     def __repr__(self):
-        return f'User {self.username}, ID: {self.id}'
+        return f'<User {self.username}, ID: {self.id}>'
+
 
 class UserSchema(Schema):
     id = fields.Int()
